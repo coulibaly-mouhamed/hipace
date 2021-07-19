cf1f62bc1332a38443d9c517c035a469d9dbc086
coulibaly-mouhamed <63076571+coulibaly-mouhamed@users.noreply.github.com>
2021-07-19 17:19:22 +0000

    Interpolation Guard Cells

--- src/fields/Fields.cpp
+++ src/fields/Fields.cpp
@@ -63,6 +63,9 @@ Fields::InterpolateGuardCells (amrex::Vector<amrex::Geometry> const& geom, const
 
     amrex::MultiFab lhs_coarse(getSlices(lev-1, WhichSlice::This), amrex::make_alias,
                                Comps[WhichSlice::This][component], 1);
+    
+    amrex::MultiFab lhs_fine(getSlices(lev, WhichSlice::This), amrex::make_alias,
+                               Comps[WhichSlice::This][component], 1);
     for (amrex::MFIter mfi( m_poisson_solver[lev]->StagingArea(),false); mfi.isValid(); ++mfi)
     {
         const amrex::Box & bx = mfi.tilebox();
@@ -74,7 +77,7 @@ Fields::InterpolateGuardCells (amrex::Vector<amrex::Geometry> const& geom, const
         const amrex::IntVect& big = bx.bigEnd();
         const auto nx_fine_high = big[0];
         const auto ny_fine_high = big[1];
-        amrex::Array4<amrex::Real>  data_array = m_poisson_solver[lev]->StagingArea().array(mfi);
+        amrex::Array4<amrex::Real>  data_array = lhs_fine.array(mfi);
         amrex::Array4<amrex::Real>  data_array_coarse = lhs_coarse.array(mfi);
         // Loop over the valid indices on the fine grid and bilinearly interpolate the boundary
         // value from the coarse grid to the outer grid points on the fine grid
@@ -82,111 +85,110 @@ Fields::InterpolateGuardCells (amrex::Vector<amrex::Geometry> const& geom, const
             bx,
             [=] AMREX_GPU_DEVICE(int i, int j , int k) noexcept
             {
-                if (i==nx_fine_low) {
+                if (i==nx_fine_low ) {
+                    //amrex::Print()<<"Before "<<data_array(i-1,j,k)<<" \n";
                     // Compute coordinate on fine grid
                     const amrex::Real x = plo[0] + (i-1+0.5_rt)*dx[0];
                     const amrex::Real y = plo[1] + (j+0.5_rt)*dx[1];
                     // index left (in x) and below (in y) of the compute coordinate on coarse grid
-                    const int idx_left = i ;
-                    const int idx_down = j ;
+                    const int idx_left = (i-1) / refinement_ratio[0];
+                    const int idx_down = j / refinement_ratio[1];
                     const amrex::Real x_left = plo_coarse[0]+(idx_left +0.5_rt)*dx_coarse[0];
                     const amrex::Real y_down = plo_coarse[1]+(idx_down +0.5_rt)*dx_coarse[1];
+
                     // Bilinear interpolation from coarse to fine grid
-                    const amrex::Real val_left_down  = data_array(idx_left  , idx_down-1  , 0);
-                    const amrex::Real val_left_up    = data_array(idx_left  , idx_down, 0);
-                    const amrex::Real val_right_up   = data_array(idx_left+1, idx_down, 0);
-                    const amrex::Real val_right_down = data_array(idx_left+1, idx_down-1  , 0);
+                    const amrex::Real val_left_down  = data_array_coarse(idx_left  , idx_down  , 0);
+                    const amrex::Real val_left_up    = data_array_coarse(idx_left  , idx_down+1, 0);
+                    const amrex::Real val_right_up   = data_array_coarse(idx_left+1, idx_down+1, 0);
+                    const amrex::Real val_right_down = data_array_coarse(idx_left+1, idx_down  , 0);
                     const amrex::Real df_x = val_right_down - val_left_down;
                     const amrex::Real df_y = val_left_up - val_left_down;
                     const amrex::Real df_xy = val_left_down + val_right_up - val_right_down
-                                                -val_left_up;
+                                            -val_left_up;
+
                     const amrex::Real boundary_value =
-                    df_x*(x-x_left)/dx[0] + df_y*(y-y_down)/dx[1] +
-                    df_xy*(x-x_left)*(y-y_down)/(dx[0]*dx[1]) + val_left_down;
-                    //amrex::Print()<<"value Before "<<data_array(i-1,j,k)<<"\n";
-                    //amrex::Print()<<"Fine\n";
+                        df_x*(x-x_left)/dx_coarse[0] + df_y*(y-y_down)/dx_coarse[1] +
+                        df_xy*(x-x_left)*(y-y_down)/(dx_coarse[0]*dx_coarse[1]) + val_left_down;
                     data_array(i-1,j,k) += boundary_value;
-                    //amrex::Print()<<"value "<<data_array(i-1,j,k)<<"\n";
+                    //amrex::Print()<<"After "<<data_array(i-1,j,k)<<" \n";
                 }
-                if(i==nx_fine_high){
+                if (i==nx_fine_high ){
                     // Compute coordinate on fine grid
                     const amrex::Real x = plo[0] + (i+1+0.5_rt)*dx[0];
                     const amrex::Real y = plo[1] + (j+0.5_rt)*dx[1];
                     // index left (in x) and below (in y) of the compute coordinate on coarse grid
-                    const int idx_left = i ;
-                    const int idx_down = j ;
+                    const int idx_left = (i+1) / refinement_ratio[0];
+                    const int idx_down = j / refinement_ratio[1];
                     const amrex::Real x_left = plo_coarse[0]+(idx_left +0.5_rt)*dx_coarse[0];
                     const amrex::Real y_down = plo_coarse[1]+(idx_down +0.5_rt)*dx_coarse[1];
+
                     // Bilinear interpolation from coarse to fine grid
-                    const amrex::Real val_left_down  = data_array(idx_left-1  , idx_down-1  , 0);
-                    const amrex::Real val_left_up    = data_array(idx_left-1  , idx_down, 0);
-                    const amrex::Real val_right_up   = data_array(idx_left, idx_down, 0);
-                    const amrex::Real val_right_down = data_array(idx_left, idx_down-1  , 0);
+                    const amrex::Real val_left_down  = data_array_coarse(idx_left  , idx_down  , 0);
+                    const amrex::Real val_left_up    = data_array_coarse(idx_left  , idx_down+1, 0);
+                    const amrex::Real val_right_up   = data_array_coarse(idx_left+1, idx_down+1, 0);
+                    const amrex::Real val_right_down = data_array_coarse(idx_left+1, idx_down  , 0);
                     const amrex::Real df_x = val_right_down - val_left_down;
                     const amrex::Real df_y = val_left_up - val_left_down;
                     const amrex::Real df_xy = val_left_down + val_right_up - val_right_down
                                             -val_left_up;
+
                     const amrex::Real boundary_value =
-                    df_x*(x-x_left)/dx[0] + df_y*(y-y_down)/dx[1] +
-                    df_xy*(x-x_left)*(y-y_down)/(dx[0]*dx[1]) + val_left_down;
-                    //amrex::Print()<<"value Before "<<data_array(i+1,j,k)<<"\n";
-                    //amrex::Print()<<"Fine2 \n";
+                        df_x*(x-x_left)/dx_coarse[0] + df_y*(y-y_down)/dx_coarse[1] +
+                        df_xy*(x-x_left)*(y-y_down)/(dx_coarse[0]*dx_coarse[1]) + val_left_down;
                     data_array(i+1,j,k) += boundary_value;
-                    //amrex::Print()<<"value "<<data_array(i+1,j,k)<<"\n";
                 }
-                if (j==ny_fine_low) {
+                if (j==ny_fine_low ) {
                     // Compute coordinate on fine grid
                     const amrex::Real x = plo[0] + (i+0.5_rt)*dx[0];
                     const amrex::Real y = plo[1] + (j-1+0.5_rt)*dx[1];
                     // index left (in x) and below (in y) of the compute coordinate on coarse grid
-                    const int idx_left = i ;
-                    const int idx_down = j ;
+                    const int idx_left = i / refinement_ratio[0];
+                    const int idx_down = (j-1) / refinement_ratio[1];
                     const amrex::Real x_left = plo_coarse[0]+(idx_left +0.5_rt)*dx_coarse[0];
                     const amrex::Real y_down = plo_coarse[1]+(idx_down +0.5_rt)*dx_coarse[1];
+
                     // Bilinear interpolation from coarse to fine grid
-                    const amrex::Real val_left_down  = data_array(idx_left  , idx_down  , 0);
-                    const amrex::Real val_left_up    = data_array(idx_left  , idx_down+1, 0);
-                    const amrex::Real val_right_up   = data_array(idx_left+1, idx_down+1, 0);
-                    const amrex::Real val_right_down = data_array(idx_left+1, idx_down  , 0);
+                    const amrex::Real val_left_down  = data_array_coarse(idx_left  , idx_down  , 0);
+                    const amrex::Real val_left_up    = data_array_coarse(idx_left  , idx_down+1, 0);
+                    const amrex::Real val_right_up   = data_array_coarse(idx_left+1, idx_down+1, 0);
+                    const amrex::Real val_right_down = data_array_coarse(idx_left+1, idx_down  , 0);
                     const amrex::Real df_x = val_right_down - val_left_down;
                     const amrex::Real df_y = val_left_up - val_left_down;
                     const amrex::Real df_xy = val_left_down + val_right_up - val_right_down
                                             -val_left_up;
+
                     const amrex::Real boundary_value =
-                    df_x*(x-x_left)/dx[0] + df_y*(y-y_down)/dx[1] +
-                    df_xy*(x-x_left)*(y-y_down)/(dx[0]*dx[1]) + val_left_down;
-                    //amrex::Print()<<"value Before "<<data_array(i,j-1,k)<<"\n";
-                    //amrex::Print()<<"Fine3 \n";
+                        df_x*(x-x_left)/dx_coarse[0] + df_y*(y-y_down)/dx_coarse[1] +
+                        df_xy*(x-x_left)*(y-y_down)/(dx_coarse[0]*dx_coarse[1]) + val_left_down;
                     data_array(i,j-1,k) += boundary_value;
-                    //amrex::Print()<<"value "<<data_array(i,j-1,k)<<"\n";
                 }
-                if(j==ny_fine_high){
+                if(j==ny_fine_high ){
                     // Compute coordinate on fine grid
                     const amrex::Real x = plo[0] + (i+0.5_rt)*dx[0];
                     const amrex::Real y = plo[1] + (j+1+0.5_rt)*dx[1];
                     // index left (in x) and below (in y) of the compute coordinate on coarse grid
-                    const int idx_left = i ;
-                    const int idx_down = j ;
+                    const int idx_left = i / refinement_ratio[0];
+                    const int idx_down = (j+1) / refinement_ratio[1];
                     const amrex::Real x_left = plo_coarse[0]+(idx_left +0.5_rt)*dx_coarse[0];
                     const amrex::Real y_down = plo_coarse[1]+(idx_down +0.5_rt)*dx_coarse[1];
+
                     // Bilinear interpolation from coarse to fine grid
-                    const amrex::Real val_left_down  = data_array(idx_left  , idx_down-1  , 0);
-                    const amrex::Real val_left_up    = data_array(idx_left  , idx_down, 0);
-                    const amrex::Real val_right_up   = data_array(idx_left+1, idx_down, 0);
-                    const amrex::Real val_right_down = data_array(idx_left-1, idx_down-1  , 0);
+                    const amrex::Real val_left_down  = data_array_coarse(idx_left  , idx_down  , 0);
+                    const amrex::Real val_left_up    = data_array_coarse(idx_left  , idx_down+1, 0);
+                    const amrex::Real val_right_up   = data_array_coarse(idx_left+1, idx_down+1, 0);
+                    const amrex::Real val_right_down = data_array_coarse(idx_left+1, idx_down  , 0);
                     const amrex::Real df_x = val_right_down - val_left_down;
                     const amrex::Real df_y = val_left_up - val_left_down;
                     const amrex::Real df_xy = val_left_down + val_right_up - val_right_down
                                             -val_left_up;
+
                     const amrex::Real boundary_value =
-                    df_x*(x-x_left)/dx[0] + df_y*(y-y_down)/dx[1] +
-                    df_xy*(x-x_left)*(y-y_down)/(dx[0]*dx[1]) + val_left_down;
-                    //amrex::Print()<<"value Before "<<data_array(i,j+1,k)<<"\n";
-                    //amrex::Print()<<"Fine4 \n";
+                        df_x*(x-x_left)/dx_coarse[0] + df_y*(y-y_down)/dx_coarse[1] +
+                        df_xy*(x-x_left)*(y-y_down)/(dx_coarse[0]*dx_coarse[1]) + val_left_down;
                     data_array(i,j+1,k) += boundary_value;
-                    //amrex::Print()<<"value "<<data_array(i,j+1,k)<<"\n";
                 }
-                //amrex::Print()<<"Done\n";
+                //amrex::Print()<<"If loop done \n";
+                
                
             });
     }
